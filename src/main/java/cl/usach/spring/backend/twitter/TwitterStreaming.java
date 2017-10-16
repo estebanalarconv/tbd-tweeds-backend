3f2ccad90c8f1b28fd16eb7388db46349684876e
conflictor <victor.maripangui@usach.cl>
2017-10-16 19:26:28 -0300

    Actualización TwitterStreaming.java
    Creada función getTweets() que busca tweets relacionado a ciertas palabras, y retorna una lista con DBObject listos para agregar a mongoDB (esta función no es en tiempo real, rescata tweets nada más); se crea función saveTweetsMongo, que recibe una lista de DBObjects, esta función agrega en la BDMOngo los datos; se crea la función findMedicalTweets(), en donde se define una lista de palabras a buscar entre los tweets con lucene, y se obtienen los ids de aquellas que la contiene (la función retorna esta lista con id's)

--- src/main/java/cl/usach/spring/backend/twitter/TwitterStreaming.java
+++ src/main/java/cl/usach/spring/backend/twitter/TwitterStreaming.java
@@ -1,28 +1,50 @@
 package cl.usach.spring.backend.twitter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.bson.Document;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import cl.usach.spring.backend.database.MongoConection;
 import cl.usach.spring.backend.lucene.Index;
+import cl.usach.spring.backend.lucene.Search;
 import twitter4j.FilterQuery;
+import twitter4j.GeoLocation;
+import twitter4j.Place;
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
+import twitter4j.User;
+import twitter4j.auth.AccessToken;
 
 public class TwitterStreaming {
 
 	private final TwitterStream twitterStream;
 	private Set<String> keywords;
+	
+	//CREACION APLICACION TWITTER CON SU ACCESS TOKEN
+	static Twitter twitter;
+	AccessToken at=new AccessToken("580554594-OgfCOpqvpxran6qlAtaCa7CMiFlDIj8Yu11Pm0YW","WxSy9Tck4dtekdnj4QEMHTAibvaFZVqe087WXiwQcvnTF");
 
 	private TwitterStreaming() {
 		this.twitterStream = new TwitterStreamFactory().getInstance();
@@ -38,6 +60,193 @@ public class TwitterStreaming {
 			e.printStackTrace();
 		}
 	}
+	
+	private static List<DBObject> getTweets() {
+		List<DBObject> documents=new ArrayList<>();
+				
+		twitter=new TwitterFactory().getInstance();
+		//twitter.setOAuthConsumer("EakDz05j85rXdkNoHAniu2K3T", "o8g470uJvqsIitPGAsEI0eCaOtoBhawP7pNI6jdcTcc9XMm4B6");
+		//twitter.setOAuthAccessToken(at);
+		
+		//SE ESTABLECEN PALABRAS A BUSCAR EN LOS TWEETS
+		Query q=new Query("marihuana OR cannabis OR weed OR 420 OR mariguana OR canabis OR pito OR skt1 OR cogollo");
+		//SE ESTABLECE CANTIDAD TE TWEETS(?)
+		q.setCount(100);
+		//SE ESTABLECE LENGUAJE DE LOS TWEETS RESCATADOS: "es"
+		q.setLang("es");
+		//SE ESTABLECEN COORDENAS DE LOCALIZACION, EN ESTE CASO SANTIAGO, CON RADIO DE 200KM
+		GeoLocation gl=new GeoLocation(-33.447487, -70.673676);
+		q.setGeoCode(gl, 200, Query.KILOMETERS);
+
+		//q.setSince("2017-10-10");
+		//q.setUntil("2017-01-03");
+		QueryResult result=null;
+		int Count=0;
+		do {
+            try {
+				result = twitter.search(q);
+				List<Status> tweets = result.getTweets();
+				String pais="";
+				String lugar="";
+	            for (int o=0;o<tweets.size();o++) {
+	            	
+	            	if(tweets.get(o).getText().contains("RT")!=true) {
+		                //NUMERO APROX DE VECES FAVORITOS
+		                int favs=tweets.get(o).getFavoriteCount();
+		                //LOCALIZACION DEL TWEET
+		                pais="";
+		                lugar="";
+		                Place localizacion= tweets.get(o).getPlace();		                
+		                if (localizacion!=null) {
+		                	pais=localizacion.getCountry();
+			                lugar=localizacion.getName();
+		                }
+		                //NUMERO DE RETWEETS
+		                int rt=tweets.get(o).getRetweetCount();
+		                //CONSEGUIR EL USUARIO
+		                User usuario=tweets.get(o).getUser();
+		                String localizacionUsuario=usuario.getLocation();
+		                String nombreusuario=usuario.getName();
+		                String nickname=usuario.getScreenName();
+		                String data=tweets.get(o).getCreatedAt().toString();
+		                
+		                //SE CREA UN OBJETO PARA LA BASE DE DATOS DE MONGO Y SE AGREGAN ATRIBUTOS
+		                //CLAVE Y VALOR
+		                BasicDBObject document = new BasicDBObject();
+		                document.put("id", tweets.get(o).getId()); 
+		                document.put("text", tweets.get(o).getText());
+		                document.put("favs", favs);
+		                if (localizacion==null) {
+		                	document.put("tweetLocalization", "");
+		                }
+		                else {
+		                	document.put("tweetLocalization", localizacion.getName());
+		                }
+		                document.put("paisTweet", pais);
+		                document.put("rt", rt);
+		                document.put("userLocation", localizacionUsuario);
+		                document.put("userName", nombreusuario);
+		                document.put("userNickname", nickname);
+		                document.put("TimeStamp", data);
+		                //SE AGREGAN LOS DATOS A LA LISTA
+		                documents.add(document);		                
+		                
+		                //IMPRESIÓN DE DATOS OBTENIDOS 
+		                //COMENTAR LUEGO
+		                System.out.println("TOTAL TWEETS RECOLECTADOS= "+Count);
+		            	Count++;
+		                System.out.println("TEXT= "+tweets.get(o).getText());
+		                System.out.println("FAVS= "+favs);
+		                System.out.println("LOCALIZACION TWEET= "+localizacion);
+		                System.out.println("PAIS= "+pais);
+		                System.out.println("LUGAR= "+lugar);
+		                System.out.println("RT= "+rt);
+		                System.out.println("LOCALIZACION USUARIO= "+localizacionUsuario);
+		                System.out.println("NOMBRE USUARIO= "+nombreusuario);
+		                System.out.println("NICKNAME USUARIO= "+nickname);
+		                System.out.println("CREATED AT= "+data);
+	            	}
+	            }
+			} catch (TwitterException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}		
+		}while ((q = result.nextQuery()) != null);
+		
+		return documents;
+	}
+	
+	private int saveTweetsMongo(List<DBObject> documents) {
+		int retorno=0;
+		try {
+			MongoClient mongoClient2;
+			mongoClient2 = new MongoClient( "localhost" , 27017 );
+			DB db = mongoClient2.getDB( "tbd1" );
+			DBCollection collection2=db.getCollection("tbd1ex");			
+			for(int d=0;d<documents.size();d++) {
+				collection2.insert(documents.get(d));
+			}
+			mongoClient2.close();	
+		}
+		catch (Exception e) {
+			return 1;
+		}
+		return retorno;
+	}
+	
+	//FUNCION QUE RETORNA LOS IDS DE LOS TWEETS CON RESPECTO A MEDICINA ENCONTRADOS
+	private List<String> findMedicalTweets() {		
+		//Asignación de palabras de categoría medicina
+				List <String> palabrasMedicina=new ArrayList<>();
+				palabrasMedicina.add("medicina*");
+				palabrasMedicina.add("medicinal*");
+				palabrasMedicina.add("estudio*");
+				palabrasMedicina.add("estudios*");
+				palabrasMedicina.add("investigación*");
+				palabrasMedicina.add("investigacion*");
+				palabrasMedicina.add("investigaciones");
+				palabrasMedicina.add("universidad*");
+				palabrasMedicina.add("universidades");
+				palabrasMedicina.add("dolor*");
+				palabrasMedicina.add("dolores");
+				palabrasMedicina.add("enfermedad");
+				palabrasMedicina.add("enfermedades");
+				palabrasMedicina.add("curativa");
+				palabrasMedicina.add("curativas");
+				palabrasMedicina.add("propiedad");
+				palabrasMedicina.add("propiedades");
+				palabrasMedicina.add("tratamiento");
+				palabrasMedicina.add("tratamientos");
+				palabrasMedicina.add("epilepsia");
+				palabrasMedicina.add("thc");
+				palabrasMedicina.add("cancer");
+				palabrasMedicina.add("cancerígena");
+				palabrasMedicina.add("cancerígenas");
+				palabrasMedicina.add("cancerigena");
+				palabrasMedicina.add("cancerigenas");
+				palabrasMedicina.add("celulas");
+				palabrasMedicina.add("células");
+				palabrasMedicina.add("esquizofrenia");
+				palabrasMedicina.add("beneficio");
+				palabrasMedicina.add("beneficios");
+				palabrasMedicina.add("beneficiable");
+				palabrasMedicina.add("beneficiables");
+				palabrasMedicina.add("memoria");
+				palabrasMedicina.add("salud");
+				palabrasMedicina.add("saludable");
+				palabrasMedicina.add("corazon");
+				palabrasMedicina.add("corazón");
+				palabrasMedicina.add("aceite");
+				palabrasMedicina.add("artitris");
+				palabrasMedicina.add("hueso");
+				palabrasMedicina.add("huesos");
+				palabrasMedicina.add("pomada");
+				palabrasMedicina.add("farmacia*");
+				palabrasMedicina.add("farmacias");
+				palabrasMedicina.add("Farmacia");
+				palabrasMedicina.add("hojas*");
+				palabrasMedicina.add("hojas");
+				palabrasMedicina.add("cultivar");				
+				
+				List<String> idEncontrados=new ArrayList<>();
+				List<String> idSearch=new ArrayList<>();
+				
+				Index index = new Index();
+				index.IndexarTweets();	
+				for (int t=0;t<palabrasMedicina.size();t++) {
+					Search search = new Search();
+					idSearch=search.SearchTweets(palabrasMedicina.get(t));
+					for (int tt=0;tt<idSearch.size();tt++) {
+						idEncontrados.add(idSearch.get(tt));
+					}
+				}
+		
+		
+		return idEncontrados;
+	}
+	
+	
+	
 
 	private void init() {
 		MongoConection mc = new MongoConection();
@@ -100,8 +309,10 @@ public class TwitterStreaming {
 		//MongoConection mc = new MongoConection();
 		//MongoCollection<Document> collection = mc.ConectarMongo();
 		//mc.mostrarColeccion(collection);
-		Index index = new Index();
-		index.IndexarTweets();	
+		//Index index = new Index();
+		//index.IndexarTweets();
+		List<DBObject> tweets=new ArrayList<>();
+		tweets=getTweets();
 			
 	}*/
 }
