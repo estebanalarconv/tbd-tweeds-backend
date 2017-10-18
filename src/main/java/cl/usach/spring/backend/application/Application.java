340cdc6d541d331592d7cccd26c3830b478fdc22
estebanalarcov <estebanalarconv@gmail.com>
2017-10-18 10:39:11 -0300

    fix application

--- src/main/java/cl/usach/spring/backend/application/Application.java
+++ src/main/java/cl/usach/spring/backend/application/Application.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"cl.citiaps.spring.backend.application", "cl.citiaps.spring.backend.rest"})
-@EntityScan("cl.citiaps.spring.backend.entities")
-@EnableJpaRepositories("cl.citiaps.spring.backend.repository")
+@ComponentScan({"cl.usach.spring.backend.application", "cl.usach.spring.backend.rest"})
+@EntityScan("cl.usach.spring.backend.entities")
+@EnableJpaRepositories("cl.usach.spring.backend.repository")
 public class Application {
 
 	public static void main(String[] args) {
